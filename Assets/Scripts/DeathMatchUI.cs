5cfe212fe6e9baf6c7e0d39d0dc102e342abb596
Steedie <Steediekid@gmail.com>
2023-04-13 18:13:58 +0200

    Added line to graph, added new settings, commented code

--- Assets/Scripts/DeathMatchUI.cs
+++ Assets/Scripts/DeathMatchUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,8 @@ public class DeathMatchUI : MonoBehaviour
     [SerializeField] private GameObject textAmountLeft, textAmountRight;
 
     [SerializeField] private Sprite arrowUp, arrowDown;
+
+    private DeathMatch thisDeathMatch;
 
     public void ShowTextAmountLeft(double amount)
     {
@@ -23,33 +26,81 @@ public class DeathMatchUI : MonoBehaviour
         textAmountRight.SetActive(true);
     }
 
-    public void SetPoint(double amount, double startAmount, float minY, float maxY, bool win, bool start)
+    //public void SetPoint(double amount, double startAmount, float minY, float maxY, bool win, bool start)
+    //{
+    //    string calculatedAmountText = $"{amount}b\n";
+    //    double percent = GetPercentageDifference(startAmount, amount);
+    //    percent = Math.Round(percent, 1);
+    //    calculatedAmountText += "<color=" + percentColor(amount, startAmount) + ">(" + percent + "%)</color>";
+
+    //    textAmount.GetComponent<Text>().text = calculatedAmountText;
+
+    //    float y = Mathf.Lerp(minY, maxY, (float)amount / 100);
+    //    imagePoint.anchoredPosition = new Vector2(imagePoint.anchoredPosition.x, y);
+
+    //    Image image = imagePoint.GetComponent<Image>();
+
+    //    if (start)
+    //    {
+    //        image.color = Color.yellow;
+    //    }
+    //    else if (win)
+    //    {
+    //        image.color = Color.green;
+    //        image.sprite = arrowUp;
+    //    }
+    //    else
+    //    {
+    //        image.color = Color.red;
+    //        image.sprite = arrowDown;
+    //    }
+    //}
+
+    // This method sets the point on the graph for the given amount
+    // It also updates the text and color of the point based on the start amount and current amount
+
+    public void SetPoint(double amount, double startAmount, float minY, float maxY, bool win, bool start, DeathMatch dm)
     {
-        string calculatedAmountText = $"{amount}b\n";
+        // Store reference to DeathMatch object
+        thisDeathMatch = dm;
+
+        // Create a StringBuilder object to build the calculatedAmountText
+        StringBuilder calculatedAmountText = new StringBuilder(16);
+
+        // Append the amount and unit to calculatedAmountText
+        calculatedAmountText.Append(amount.ToString("F1"));
+        calculatedAmountText.AppendLine("b");
+
+        // Calculate the percentage difference between startAmount and amount
         double percent = GetPercentageDifference(startAmount, amount);
+
+        // Round the percent to one decimal place
         percent = Math.Round(percent, 1);
-        calculatedAmountText += "<color=" + percentColor(amount, startAmount) + ">(" + percent + "%)</color>";
 
-        textAmount.GetComponent<Text>().text = calculatedAmountText;
+        // Append the percent with color to calculatedAmountText
+        calculatedAmountText.AppendFormat("<color={0}>{1}%</color>", percentColor(amount, startAmount), percent);
 
+        // Set the text of the textAmount object to calculatedAmountText
+        textAmount.GetComponent<Text>().text = calculatedAmountText.ToString();
+
+        // Calculate the y position of the imagePoint based on the amount and range of y values
         float y = Mathf.Lerp(minY, maxY, (float)amount / 100);
+
+        // Set the anchored position of imagePoint to the calculated y value
         imagePoint.anchoredPosition = new Vector2(imagePoint.anchoredPosition.x, y);
 
+        // Get a reference to the Image component of imagePoint
         Image image = imagePoint.GetComponent<Image>();
 
+        // Set the color and sprite of the image based on whether it is the start point or a win/loss point
         if (start)
         {
             image.color = Color.yellow;
         }
-        else if (win)
-        {
-            image.color = Color.green;
-            image.sprite = arrowUp;
-        }
         else
         {
-            image.color = Color.red;
-            image.sprite = arrowDown;
+            image.color = win ? Color.green : Color.red;
+            image.sprite = win ? arrowUp : arrowDown;
         }
     }
 
@@ -61,13 +112,6 @@ public class DeathMatchUI : MonoBehaviour
             return "yellow";
         else
             return "red";
-    }
-
-    private double CalculatePercentageIncrease(double originalValue, double newValue)
-    {
-        double difference = newValue - originalValue;
-        double percentageIncrease = (difference / originalValue) * 100f;
-        return Mathf.Abs((float)percentageIncrease);
     }
 
     private double GetPercentageDifference(double a, double b)
@@ -92,5 +136,11 @@ public class DeathMatchUI : MonoBehaviour
     public void OnMouseExit()
     {
         textAmount.SetActive(false);
+    }
+
+    public void OnPointerClick()
+    {
+        if (thisDeathMatch != null)
+            FindObjectOfType<GameManager>().EditDeathMatch(thisDeathMatch);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,6 @@ public class GameManager : MonoBehaviour
     [SerializeField, Tooltip("(b) for example, 0.8 is 0.8 billion")]
     private double startingAmount;
 
-    //[SerializeField, Tooltip("Transform that holds the list of deathmatches in the settings UI")] 
-    //private Transform verticalDeathmatchHolder;
-
     [SerializeField, Tooltip("Transform that holds the list of deathmatches in the main graph UI")] 
     private Transform horizontalDeathmatchHolder;
 
@@ -27,15 +24,26 @@ public class GameManager : MonoBehaviour
     [SerializeField, Tooltip("Win and Loss text")] 
     private Text textWins, textLosses;
 
-
     private List<DeathMatch> matches = new List<DeathMatch>();
+
+    private Camera mainCamera;
+
+    private DeathMatch editingDeathMatch = null;
+
+    [SerializeField]
+    private GameObject settingsEditDeathMatch;
+    [SerializeField]
+    private InputField editDmInputField;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
 
     public void ApplyStartingAmount()
     {
-        double amount = 0;
-        if (double.TryParse(startingAmountInputField.text, out amount))
+        if (double.TryParse(startingAmountInputField.text, out startingAmount))
         {
-            startingAmount = amount;
             UpdateGraph();
         }
     }
@@ -51,25 +59,29 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // This method is used to update the DeathMatch graph with the current state of the matches list
     private void UpdateGraph()
     {
-        // Clear graph
+        // Clear the graph by destroying all child objects in the horizontalDeathmatchHolder game object
         for (int i = horizontalDeathmatchHolder.childCount - 1; i >= 0; i--)
         {
             Destroy(horizontalDeathmatchHolder.GetChild(i).gameObject);
         }
 
-        // Starting point (starting amount)
+        // Instantiate a starting point for the graph and set its properties
         GameObject startingPoint = Instantiate(deathmatchPrefab, horizontalDeathmatchHolder);
-        startingPoint.GetComponent<DeathMatchUI>().SetPoint(startingAmount, startingAmount, -450, 450, false, true);
+        startingPoint.GetComponent<DeathMatchUI>().SetPoint(startingAmount, startingAmount, -450, 450, false, true, null);
         startingPoint.GetComponent<DeathMatchUI>().ShowTextAmountLeft(startingAmount);
 
+        // Declare and initialize variables to store the current amount, number of wins, and number of losses
         double currentAmount = startingAmount;
         int wins = 0;
         int losses = 0;
 
+        // Loop through each DeathMatch object in the matches list
         for (int i = 0; i < matches.Count; i++)
         {
+            // Update the current amount based on the win/loss and amount of the current DeathMatch object
             if (matches[i].win)
             {
                 currentAmount += matches[i].amount;
@@ -81,20 +93,144 @@ public class GameManager : MonoBehaviour
                 losses++;
             }
 
+            // Instantiate a new DeathMatchUI object for the current DeathMatch object and set its properties
             GameObject newMatch = Instantiate(deathmatchPrefab, horizontalDeathmatchHolder);
-            newMatch.GetComponent<DeathMatchUI>().SetPoint(currentAmount, startingAmount, -450, 450, matches[i].win, false);
+            newMatch.GetComponent<DeathMatchUI>().SetPoint(currentAmount, startingAmount, -450, 450, matches[i].win, false, matches[i]);
+
+            // If the current DeathMatch object is the last one in the matches list, show the amount on the right side of the graph
             if (i == matches.Count - 1)
                 newMatch.GetComponent<DeathMatchUI>().ShowTextAmountRight(currentAmount);
         }
 
+        // Set the text of the wins and losses UI elements
         textWins.text = $"W: {wins}";
         textLosses.text = $"L: {losses}";
+
+        // Call the RenderLineAtEndOfFrame coroutine to render the line connecting the points on the graph
+        StartCoroutine(RenderLineAtEndOfFrame());
     }
 
     public void OnSpreadSliderValueChanged(float value)
     {
         horizontalDeathmatchHolder.GetComponent<HorizontalLayoutGroup>().spacing = value;
+        StartCoroutine(RenderLineAtEndOfFrame());
     }
+
+    [SerializeField] 
+    private LineRenderer lineRenderer;
+
+    public void OnHorizontalScrollChanged()
+    {
+        StartCoroutine(RenderLineAtEndOfFrame());
+    }
+
+    public void OnRenderLineToggle(bool value)
+    {
+        lineRenderer.enabled = value;
+        StartCoroutine(RenderLineAtEndOfFrame());
+    }
+
+    IEnumerator RenderLineAtEndOfFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        RenderLine();
+    }
+
+    // This method is used to render a line connecting the points on the DeathMatch graph
+    private void RenderLine()
+    {
+        // Check if the lineRenderer is enabled, if not return
+        if (!lineRenderer.enabled) return;
+
+        // Get the number of child objects in the horizontalDeathmatchHolder game object
+        int childCount = horizontalDeathmatchHolder.childCount;
+
+        // Declare and initialize an array of Vector3 to store the points on the graph
+        Vector3[] points = new Vector3[childCount];
+
+        // Loop through each child object in the horizontalDeathmatchHolder game object
+        for (int i = 0; i < childCount; i++)
+        {
+            // Get the Transform component of the current child object
+            Transform child = horizontalDeathmatchHolder.GetChild(i);
+
+            // Get the position of the child object in world space using the mainCamera
+            Vector3 point = mainCamera.ScreenToWorldPoint(new Vector3(child.GetChild(0).GetComponent<RectTransform>().position.x, child.GetChild(0).GetComponent<RectTransform>().position.y, mainCamera.nearClipPlane));
+
+            // Add the point to the points array
+            points[i] = point;
+        }
+
+        // Set the number of positions in the lineRenderer to the number of child objects in the horizontalDeathmatchHolder game object
+        lineRenderer.positionCount = childCount;
+
+        // Set the positions of the lineRenderer to the points array
+        lineRenderer.SetPositions(points);
+    }
+
+    #region EDIT
+    // This method is used to edit a DeathMatch object
+    public void EditDeathMatch(DeathMatch dm)
+    {
+        // Check if the DeathMatch object being edited is not the same as the current editingDeathMatch
+        if (dm != editingDeathMatch)
+        {
+            // Set the editingDeathMatch to the current DeathMatch object being edited
+            editingDeathMatch = dm;
+
+            // Set the text of the editDmInputField based on whether the DeathMatch object has won or lost
+            // If the DeathMatch object has won, the text will be an empty string
+            // If the DeathMatch object has lost, the text will be a negative number followed by the amount lost
+            editDmInputField.text = (dm.win ? "" : "-") + dm.amount;
+
+            // Activate the settingsEditDeathMatch game object
+            settingsEditDeathMatch.SetActive(true);
+        }
+        else
+        {
+            // Set the editingDeathMatch to null and deactivate the settingsEditDeathMatch game object
+            editingDeathMatch = null;
+            settingsEditDeathMatch.SetActive(false);
+        }
+    }
+
+    // This method is called when the Edit Apply button is clicked
+    public void Button_EditApply()
+    {
+        // Declare and initialize a variable to store the amount entered in the editDmInputField
+        double amount = 0;
+
+        // Try to parse the text in the editDmInputField as a double
+        if (double.TryParse(editDmInputField.text, out amount))
+        {
+            // Set the amount of the editingDeathMatch object to the absolute value of the parsed amount
+            editingDeathMatch.amount = Mathf.Abs((float)amount);
+
+            // Check if the parsed amount is negative
+            if (amount < 0)
+                editingDeathMatch.win = false; // If it is negative, set win to false (i.e. the DeathMatch object lost)
+            else
+                editingDeathMatch.win = true; // If it is not negative, set win to true (i.e. the DeathMatch object won)
+
+            // Call the UpdateGraph method to update the graph with the changes made to the DeathMatch object
+            UpdateGraph();
+
+            // Deactivate the settingsEditDeathMatch game object and set editingDeathMatch to null
+            settingsEditDeathMatch.SetActive(false);
+            editingDeathMatch = null;
+        }
+    }
+
+    public void Button_EditDelete()
+    {
+        matches.Remove(editingDeathMatch);
+        editingDeathMatch = null;
+        settingsEditDeathMatch.SetActive(false);
+        UpdateGraph();
+    }
+
+    #endregion
+
 }
 
 public class DeathMatch
